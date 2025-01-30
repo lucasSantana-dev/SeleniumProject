c26008a8e8dd732c9a96fb679488c3f78da2ee9d
Lucas Santana <lucassantan017@gmail.com>
2025-01-30 00:26:48 -0300

    Criação de classes Page(DressesPage, LoginPage, MyAccountPage). Refatoração de métodos para dentro destas classes.

---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,28 @@
+package pages;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import utils.Utils;
+import java.time.Duration;
+
+public class LoginPage {
+
+    Duration tempo = Duration.ofSeconds(5);
+
+    String loginButton = "//a[@class='login']";
+    String emailInput = "//input[@id='email']";
+    String passwdInput = "//input[@id='passwd']";
+    String submitLoginButton = "//button[@id='SubmitLogin']";
+
+    public void fazerLogin() {
+        Utils.initDriver().get("http://www.automationpractice.pl/index.php");
+        Utils.esperarElemento(tempo, By.xpath(loginButton));
+        Utils.clickButton(loginButton);
+        Utils.esperarElemento(tempo, By.xpath(emailInput));
+        Utils.validarTitulo("Login - My Shop");
+        Utils.sendKeys(emailInput, "automatedTest@qa.com");
+        Utils.sendKeys(passwdInput, "12345");
+        Utils.clickButton(submitLoginButton);
+    }
+
+}
---
+++ src/test/java/pages/MyAccountPage.java
@@ -0,0 +1,18 @@
+package pages;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import utils.Utils;
+
+public class MyAccountPage {
+
+    //elements path
+    String myAccountTitle = "//h1[text()='My account']";
+    String logo = "//img[contains(@alt,'My Shop')]";
+
+    public void validarPaginaMyAccount() {
+            Utils.esperarElemento(Utils.tempo, By.xpath(myAccountTitle));
+            Utils.validarTitulo("My account - My Shop");
+            Utils.clickButton(logo);
+        }
+}
